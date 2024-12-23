5d591f1b95e9a403341aa8876fd6c22798104adb
Mirphon <sviaznoy1006@gmail.com>
2024-12-23 13:45:43 +0300

    Delete L2.3.py

--- L2.3.py
+++
@@ -1,8 +0,0 @@
-def is_prime(number):
-    d = set()
-    for i in range(1, int(number**0.5) + 1):
-        if number % i == 0:
-            d.add(number//i)
-            d.add(i)
-    return print(len(d) <= 2)
-is_prime(3)
