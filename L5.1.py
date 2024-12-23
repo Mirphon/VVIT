1516e866387f6ed061c2ac898f9ae7b85d851773
Mirphon <sviaznoy1006@gmail.com>
2024-12-23 13:44:22 +0300

    Delete L5.1.py

--- L5.1.py
+++
@@ -1,9 +0,0 @@
-class Book:
-    def __init__(self, tittle, author, year):
-        self.tittle = tittle
-        self.author = author
-        self.year = year
-    def get_info(self):
-        return f'Название книги: {self.tittle}. Автор: {self.author}. Год издания: {self.year}.'
-obj = Book('Война и мир', 'Л. Толстой', 10)
-print(obj.get_info())
